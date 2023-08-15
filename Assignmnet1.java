5d85752dbe1d1ad937582d1330ba6b0b2dbc5be7
Pasan sadeepa <pasansadeepa222@gmail.com>
2023-08-15 22:55:36 +0530

    initial commit

--- Assignmnet1.java
+++ Assignmnet1.java
@@ -1,4 +1,4 @@
-import java.io.StringReader;
+
 import java.util.Scanner;
 
 public class Assignmnet1{
@@ -29,6 +29,69 @@ private static final Scanner SCANNER = new Scanner(System.in);
         String[] WithdrawalsNmae = new String [0];
 
         String screen = DASHBOARD;
+
+        mainLoop:
+        do{
+            final String APP_TITLE = String.format("%s%s%s",
+            COLOR_BLUE_BOLD, screen, RESET);
+            System.out.println(CLEAR);
+            System.out.println("\t" + APP_TITLE + "\n");
+            switch(screen){
+                case DASHBOARD: 
+                    System.out.println("\t[1]. Create new Account");
+                    System.out.println("\t[2]. Deposits");
+                    System.out.println("\t[3]. Withdrawls");
+                    System.out.println("\t[4]. Transfer");
+                    System.out.println("\t[3]. Check Account Balance");
+                    System.out.println("\t[3]. Delete Account");
+                    System.out.println("\t[4]. Exit\n");
+                    System.out.print("\tEnter an option to continue: ");
+                    int option = SCANNER.nextInt();
+                    SCANNER.nextLine();
+                    switch (option){
+                        case 1: screen = CREATE_NEW_ACCOUNT; break;
+                        case 2: screen = DEPOSITS; break;
+                        case 3: screen = WITHDRAWLS; break;
+                        case 4: screen = TRANSFER;break;   
+                        case 5: screen = CHECK_ACCOUNT_BALANCE;break;
+                        case 6: screen =  DELETE_ACCOUNT;break;  
+                        case 7: System.out.println(CLEAR); System.exit(0);
+                        default: continue;
+                    }
+                    break;
+                case ADD_CUSTOMER:
+                    String id;
+                    String name;
+                    boolean valid;
+                    // ID Validation
+                    do {
+                        valid = true;
+                        System.out.print("\tEnter New Customer ID: ");  // C-ac
+                        id = SCANNER.nextLine().toUpperCase().strip();
+                        if (id.isBlank()){
+                            System.out.printf(ERROR_MSG, "ID can't be empty");
+                            valid = false;
+                        }else if (!id.startsWith("C-") || id.length() < 3){
+                            System.out.printf(ERROR_MSG, "Invalid ID format");
+                            valid = false;
+                        }else{
+                            String number = id.substring(2);
+                            for (int i = 0; i < number.length(); i++) {
+                                if (!Character.isDigit(number.charAt(i))){
+                                    System.out.printf(ERROR_MSG, "Invalid ID format");
+                                    valid = false;
+                                    break;
+                                }
+                            }
+                            for (int i = 0; i < customerIds.length; i++) {
+                                if (customerIds[i].equals(id)){
+                                    System.out.printf(ERROR_MSG, "Customer ID already exists");
+                                    valid = false;
+                                    break;
+                                }
+                            }    
+                        }
+                    }while (!valid);
         
 
 
